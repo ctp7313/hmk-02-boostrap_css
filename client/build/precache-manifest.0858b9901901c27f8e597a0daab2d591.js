b1399d737b418c1bf37689e0a86d76f6ef2693cc
Chris Parker <ctp7313@gmail.com>
2020-11-22 23:20:16 -0700

    last build before deployment

---
+++ client/build/precache-manifest.0858b9901901c27f8e597a0daab2d591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41616a127b9708730366dd18f236972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af5be6b73e3144063d58",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "40c7114ef7913cf37c59",
+    "url": "/static/js/2.f3db3991.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f3db3991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5be6b73e3144063d58",
+    "url": "/static/js/main.1e42a01f.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
