7961a1e722b79dbf2b3a968b3a9e888ff89ab9ad
Chris Parker <ctp7313@gmail.com>
2020-11-22 22:25:11 -0700

    Completed portfolio

--- create-react-express/client/src/pages/Contact.js
+++ create-react-express/client/src/pages/Contact.js
@@ -1,94 +1,49 @@
 import React, { useState } from 'react';
-import Header from '../components/Header';
 import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from '../components/Jumbotron'
+
 
 
 export default function Contact() {
   return(
-    <Container>
-      <Header headerName={"Contact Me"} subLine={"Let's Work Together"}/>
- 
-    </Container>
+    <div>
+      <Jumbotron main="Contact Me" subline="Let's make something together"/>
+      <Container>
+        <div className="row d-flex justify-content-center">
+          <Col size="md-5">
+            <form>
+              <div class="form-group">
+                <label for="contactName">Name</label>
+                <input type="name" class="form-control" id="contactName" placeholder="First and last name"/>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail">Email</label>
+                <input type="email" class="form-control" id="inputEmail" placeholder="Enter email"/>
+                <small class="form-text text-muted">I'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="messageArea">Message</label>
+                <textarea class="form-control" id="messageArea" rows="3" placeholder="Questions? Have a fun project in mind? Leave your details!"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </Col>
+          <div class="card text-white mb-3">
+            <div className="card-body">
+              <h6>Feel free to reach me at the following:</h6>
+              <ul>
+                <li><a href="tel:206-643-0293">206.643.0293</a></li>
+                <li><a href = "mailto: ctp7313@gmail.com">CTP7313@gmail.com</a></li>
+                <li><a href="https://www.linkedin.com/in/chrs-prkr/" target="_blank">LinkedIn</a></li>
+                <li><a href="https://github.com/ctp7313" target="_blank">Github</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
-}// <!DOCTYPE html>
-// <html lang="en">
-
-// <head>
-//     <meta charset="utf-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    <link rel="stylesheet" href="assets/style.css">
-//     <link rel="stylesheet" href="assets/style.css">
-//     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-//     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-//     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
-    
-//     <title>Chris Parker - Web Developer | Home</title>
-// </head>
-
-// <body>
-// <!-- Navbar  -->
-//   <nav class="navbar navbar-expand-sm navbar-dark bg-dark">        
-//     <a class="navbar-brand" href="index.html">Chris Parker</a>
-//     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-//         <span class="navbar-toggler-icon"></span>
-//       </button>
-//       <div class="collapse navbar-collapse" id="navbarMenu">
-//         <div class="navbar-nav ml-auto">
-//         <a class="nav-item nav-link" href="index.html">Home</a>
-//         <a class="nav-item nav-link" href="portfolio.html">Portfolio</a>
-//         <a class="nav-item nav-link active" href="contact.html">Contact <span class="sr-only">(current)</span></a>
-//       </div>
-//     </div>
-//   </nav>
-// <!-- End of Navbar -->
-
-// <!-- Contact Section -->
-//   <div class="container mt-3">
-//     <div class="row">
-//       <div class="col-sm-4">
-//         <h4>Contact</h4>
-//       </div>
-//     </div>
-//     <hr>
-
-//     <div class="row">
-//       <div class="col-sm-8">
-//         <form>
-//           <!-- First and last name -->
-//           <div class="form-group">
-//             <label for="contactName">Name</label>
-//             <input type="name" class="form-control" id="contactName" placeholder="First and last name">
-//           </div>
-//           <!-- Email -->
-//           <div class="form-group">
-//             <label for="inputEmail">Email</label>
-//             <input type="email" class="form-control" id="inputEmail" placeholder="Enter email">
-//             <small class="form-text text-muted">I'll never share your email with anyone else.</small>
-//           </div>
-//           <!-- Message -->
-//           <div class="form-group">
-//             <label for="messageArea">Message</label>
-//             <textarea class="form-control" id="messageArea" rows="3" placeholder="Questions? Have a fun project in mind? Leave your details!"></textarea>
-//           </div>
-//           <!-- Submit button -->
-//           <button type="submit" class="btn btn-primary">Submit</button>
-//         </form>
-//       </div>
-//       <div class="col-sm-4">
-//         <p>Feel free to reach me at the following:</p>
-//         <ul>
-//           <li><a href="tel:206-643-0293">206.643.0293</a></li>
-//           <li><a href = "mailto: ctp7313@gmail.com">CTP7313@gmail.com</a></li>
-//           <li><a href="https://www.linkedin.com/in/chrs-prkr/" target="_blank">LinkedIn</a></li>
-//         </ul>
-//       </div>
-//     </div>
-//   </div>
+}
 
 
-// <!-- Footer -->
-// <footer id="footer">
-// </footer>
 
-// </body>
-// </html>
--- create-react-express/client/src/pages/Portfolio.js
+++ create-react-express/client/src/pages/Portfolio.js
@@ -1,26 +1,24 @@
 import React from "react";
-import CardColumns from "react-bootstrap/CardColumns";
-import Header from '../components/Header';
 import data from '../components/projects/data.json'
-import {Container} from '../components/Grid'
+import {Container, Row} from '../components/Grid'
+import Jumbotron from "../components/Jumbotron/index";
 
-const  project = data.map((data) => {
+const project = data.map((data) => {
   return  (
-        <div className="card text-white mb-4" key={data.id}>
+        <div className="card text-white mb-3 col-4" key={data.id}>
                 <div className="card-body">
                   <img  className="img-fluid rounded mx-auto d-block" 
                         src={data.image}
                         alt={data.title} 
-                        style={{  height: 300, 
-                                  clear: "both", 
-                                  paddingBottom: 10}} />
-                  <div className="d-flex w-100 justify-content-between">
+                        style={{paddingBottom: 10}} />
+                  <div className="d-flex justify-content-between">
                     <h3>{data.title}</h3>
                   </div>
                   <p>{data.desc}</p>
-                  <button className="btn btn btn-primary"><a className="text-white" href={data.url}>Site</a></button>
-                  <button className="btn btn btn-primary"><a className="text-white" href={data.github}>Code</a></button>
-
+                  <Row className="">
+                    <button className="btn btn btn-primary"><a className="text-white" href={data.url}>Site</a></button>
+                    <button className="btn btn btn-primary"><a className="text-white" href={data.github}>Code</a></button>
+                  </Row>       
                 </div>
               </div>
   )
@@ -28,13 +26,13 @@ const  project = data.map((data) => {
 
 export default function Portfolio() {
   return(
-    <>
-      <div className="container container-fluid">
-        <Header headerName={"Portfolio"} subLine={"Current & Past Projects"}/>
-      </div>
+    <div>
+      <Jumbotron main="Portfolio" subline="Past & Current Projects" />
       <Container>
-        <CardColumns> {project} </CardColumns> 
-      </Container>
-    </>
+        <Row>
+          {project}
+        </Row>
+      </Container> 
+    </div>
   )
 }
